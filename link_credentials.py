afea43b4ddf4568cc70f3ea8967027527381b8b8
Satyashree G Rao <147239703+satyarao08@users.noreply.github.com>
2026-01-12 14:25:51 +0530

    Add files via upload

---
+++ link_credentials.py
@@ -0,0 +1,6 @@
+import os
+
+# Use the exact long name shown in your sidebar
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "ghost-demand-hackathon-6c69f097d879.json"
+
+print("Credentials linked successfully.")
